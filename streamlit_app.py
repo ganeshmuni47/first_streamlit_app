f77256885aa7cb9808bc1faeb8de1c90400b5000
Ganesh Muni <89179953+ganeshmuni47@users.noreply.github.com>
2023-06-06 16:22:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,4 +7,9 @@ streamlit.text('🥗 kale,Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled free-range egg')
 streamlit.text('🥑 🍞 Avocado toast')
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.header('🍌🥭Fruit Smoothie 🥝🍇')
+streamlit.text('Oaty banana smoothie')
+streamlit.text('Peachy oat smoothie')
+streamlit.text('Strawberry banana smoothie')
+streamlit.text('Blueberry lavender smoothie')
+
