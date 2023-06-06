02b3cb815225469d311a8d6e523762ade342f67d
Ganesh Muni <89179953+ganeshmuni47@users.noreply.github.com>
2023-06-06 16:38:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,4 +15,6 @@ streamlit.text('Blueberry lavender smoothie')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
+
 
